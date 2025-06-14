a55ef1d36095e906dbe44afeacf69e1b90c5d813
Anki <ankitanaidu@Ankitas-MacBook-Air.local>
2025-06-14 08:56:31 +0530

    Add workspace tag and make variables required

--- resources.tf
+++ resources.tf
@@ -18,7 +18,7 @@ locals {
   common_tags = {
     Environment = var.environment
     BillingCode = var.billing_code
-    Workspace = terraform.workspace
+    Workspace   = terraform.workspace
   }
 }
 
